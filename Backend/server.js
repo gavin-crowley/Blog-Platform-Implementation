7ce3fd7da36d03fc0445933e52c0bd34db5b3049
gavin-crowley <42537387+gavin-crowley@users.noreply.github.com>
2020-03-23 13:38:54 +0400

    13 sign in data in DB

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
 // bring routes
 const blogRoutes = require('./routes/blog');
@@ -11,16 +12,22 @@ const authRoutes = require('./routes/auth');
 // app
 const app = express();
 
+// db
+mongoose
+    .connect(process.env.DATABASE, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
+    .then(() => console.log('DB connected'))
+    .catch(err => {
+        console.log(err);
+    });
+
 // middlewares
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
-
 // cors
 if (process.env.NODE_ENV === 'development') {
     app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 }
-
 // routes middleware
 app.use('/api', blogRoutes);
 app.use('/api', authRoutes);
